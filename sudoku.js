d2b2e1753cd9ba86bb58c8f542e610c7f3353560
LauriAL <lauri.a.lahtinen@hotmail.com>
2024-02-29 11:14:52 +0200

    Added number generator to create different boards, but with same difficulty

--- sudoku.js
+++ sudoku.js
@@ -1,31 +1,86 @@
+function numGen() {
+    let grid = Array.from({ length:9 }, () => Array(9).fill(' '));
+    let num = "123456789";
+
+    for(let i = 0; num.length > 0 && i < 9; i++) {
+        let index = Math.floor(Math.random() * num.length);
+        let val = num.substring(index, index + 1);
+        grid[0][i] = val;
+        num = num.replace(val, '');
+    }
+
+    function solver() {
+        for(let i = 0; i < 9; i++) {
+            for(let j = 0; j < 9; j++) {
+
+                if(grid[i][j] == ' ') {
+                    for(let val = 1; val < 10; val++) {
+
+                        if(!(rowCheck(val, i, grid) || colCheck(val, j, grid) || subgrid(val, i, j, grid))) {
+                            grid[i][j] = val;
+                            if(solver()) {
+                                return true;
+                            }
+                            grid[i][j] = ' ';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    solver();
+
+    return grid;
+}
+
+function rowCheck(val, index, grid) {
+    return grid[index].includes(val);
+}
+
+function colCheck(val, index, grid) {
+    for(let i = 0; i < 9; i++) {
+        if(grid[i][index] == val) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function subgrid(val, row, col, grid) {
+    for(let i = Math.floor(row/3) * 3; i < (Math.floor(row/3) * 3) + 3; i++) {
+        for(let j = Math.floor(col/3) * 3; j < (Math.floor(col/3) * 3) + 3; j++) {
+            if(grid[i][j] == val) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function removeValues(dif, sol) {
+    let board = sol;
+
+    for(let i = 1; i < dif; i++) {
+        let row = Math.floor(Math.random() * 9);
+        let col = Math.floor(Math.random() * 9);
+        board[row][col] = ' ';
+    }
+    return board;
+}
+
 var numSelected = null;
-var tileSelected = null;
 
 var errors = 0;
 
-var board = [
-    "--74916-5",
-    "2---6-3-9",
-    "-----7-1-",
-    "-586----4",
-    "--3----9-",
-    "--62--187",
-    "9-4-7---2",
-    "67-83----",
-    "81--45---"
-]
+var solution = numGen();
+var get = JSON.parse(JSON.stringify(solution));
+var board = removeValues(50, get);
 
-var solution = [
-    "387491625",
-    "241568379",
-    "569327418",
-    "758619234",
-    "123784596",
-    "496253187",
-    "934176852",
-    "675832941",
-    "812945763"
-]
+solution.forEach((i) => console.log(i + "\n"));
+board.forEach((i) => console.log(i + "\n"));
 
 window.onload = function() {
     setGame();
@@ -47,8 +102,8 @@ function setGame() {
     for (let r = 0; r<9; r++) {
         for (let c = 0; c<9; c++) {
             let tile = document.createElement("div");
-            tile.id = r.toString() + "-" + c.toString();
-            if (board[r][c] != "-") {
+            tile.id = r.toString() + " " + c.toString();
+            if (board[r][c] != " ") {
                 tile.innerText = board[r][c];
                 tile.classList.add("tile-start");
             }
@@ -80,7 +135,7 @@ function selectTile() {
         }
         
         // "0-0", "0-1", ... "8-8"
-        let coords = this.id.split("-"); //["0", "0"]
+        let coords = this.id.split(" "); //["0", "0"]
         let r = parseInt(coords[0]);
         let c = parseInt(coords[1]);
 
